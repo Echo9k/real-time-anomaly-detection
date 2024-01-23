040fd74e9932cafb9409ff81b91c2634fdff84c0
Guillermo Alcántara <32722692+Echo9k@users.noreply.github.com>
2024-01-23 17:54:35 -0600

    Compose with service, main with metrics tracking
    Compose with anomaly service \ Grafana admin credentials as a file. \ Prometheus metrics

---
+++ monitoring/README.md
@@ -0,0 +1,7 @@
+```bash
+export GF_USER=admin
+export GF_PASSWORD=admin
+export CY_USER=admin
+export CY_PASSWORD='$2a$14$vOY7yOGQI9cswmeh5wO1suectLqLzyUhexNN1sr0zMy2BUt9Wggy2'
+```
+
--- service/main.py
+++ service/main.py
@@ -1,9 +1,10 @@
-import logging
-from fastapi import FastAPI, HTTPException
+import logging, time
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 from typing import List, Optional
 import joblib
 import numpy as np
+from prometheus_client import generate_latest, make_asgi_app, Counter, Histogram, CONTENT_TYPE_LATEST
 
 
 # Configure logging
@@ -17,6 +18,18 @@ logging.basicConfig(level=logging.INFO,
 # Load the trained Isolation Forest model
 logging.info("Loading the model...")
 model = joblib.load("/app/models/model.pkl")
+
+# Create Prometheu' metrics
+prediction_counter = Counter("prediction_counter", "Number of prediction endpoint calls")
+model_information_counter = Counter("model_information_counter", "Number of model_information endpoint calls")
+predictions_output = Histogram('predictions_output', 'Histogram for tracking the model predictions response times', buckets=(0.1, 0.5, 1, 5, 10, float('inf')))
+
+# Histogram to track the score sample predictions
+predictions_scores = Histogram('predictions_scores', 'Histogram for tracking the scores of the model predictions')
+
+# Histogram to track the function's latency
+predictions_latency = Histogram('predictions_latency', 'Histogram for tracking the latency of the prediction function', buckets=(0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, float('inf')))
+
 
 # Create the FastAPI app
 logging.info("Creating the FastAPI app...")
@@ -68,6 +81,9 @@ async def prediction(request: PredictionRequest):
     Raises:
         HTTPException: If there is an error during prediction.
     """
+    prediction_counter.inc()
+    start_time = time.time()  # Start time measurement
+
     try:
         feature_vector = np.array([request.feature_vector])
         prediction = model.predict(feature_vector)[0]
@@ -77,13 +93,24 @@ async def prediction(request: PredictionRequest):
             anomaly_score = model.decision_function(feature_vector)[0]
             response["anomaly_score"] = anomaly_score
 
+            # Record the score in the histogram
+            predictions_scores.observe(anomaly_score)
+
         logging.info("Prediction request processed successfully.")
+
+        # Record the prediction response time
+        predictions_output.observe(time.time() - start_time)
+
+        # Calculate and record the function's latency
+        predictions_latency.observe(time.time() - start_time)  # Record latency
+
         return PredictionResponse(**response)
 
     except Exception as e:
         # Log the error at ERROR level
         logging.error(f"Error in prediction: {str(e)}", exc_info=True)
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.get("/model_information")
 async def model_information():
@@ -96,6 +123,7 @@ async def model_information():
     Raises:
         HTTPException: If there is an error retrieving the model parameters.
     """
+    model_information_counter.inc()
     try:
         params = model.get_params()
         return ModelInformation(parameters=params)
@@ -122,3 +150,11 @@ async def status():
     """
     logging.info("Status check accessed")
     return {"status": "running"}
+
+@app.get("/metrics")
+async def metrics():
+    return Response(content=generate_latest(), media_type=CONTENT_TYPE_LATEST)
+
+
+# Create the ASGI app using make_asgi_app()
+app.mount("/metrics", make_asgi_app())
